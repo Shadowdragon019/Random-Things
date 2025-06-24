dd262a2160186db9179f18aa9e3b2958b159e0ec
Creature Girl <wolfboycoolkid@gmail.com>
2025-06-24 01:08:30 -0500

    Added todos

--- src/main/java/lol/roxxane/random_things/Rt.java
+++ src/main/java/lol/roxxane/random_things/Rt.java
@@ -3,18 +3,19 @@ package lol.roxxane.random_things;
 import com.mojang.logging.LogUtils;
 import com.tterrag.registrate.Registrate;
 import lol.roxxane.random_things.blocks.RtBlocks;
+import lol.roxxane.random_things.config.RtServerConfig;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 //fill ~15 ~15 ~15 ~-15 ~-15 ~-15 glass replace #random_things:all_replaceables
 // I wanna gen stuff dynamicly with - https://www.curseforge.com/minecraft/mc-mods/dynamic-asset-generator
 // Or just render the block below it with the ore texture on top
-// TODO: Dungeon that uses Unstable Stone
-// TODO: Make it not collapse if mined with silk touch
-// TODO: Make it obtainable with silk touch
+// TODO: Dungeon that uses Unstable Stone (maybe combo it with deadly walls? like a stone that explodes when broken or a block is placed next to it)
+// TODO: Unstable stone worldgen (64 block big blobs that replace any stone)
 @Mod(Rt.ID)
 public class Rt {
 	public static final String ID = "random_things";
@@ -25,11 +26,14 @@ public class Rt {
 		var forge_event_bus = context.getModEventBus();
 		MinecraftForge.EVENT_BUS.register(this);
 
+		context.registerConfig(ModConfig.Type.SERVER, RtServerConfig.SPEC);
+
 		RtBlocks.register();
 
 		forge_event_bus.addListener(RtEvents::fill_creative_tabs);
 	}
 
+	@SuppressWarnings("unused")
 	public static void log(Object object) {
 		LOGGER.info(object.toString());
 	}
--- src/main/java/lol/roxxane/random_things/blocks/UnstableStone.java
+++ src/main/java/lol/roxxane/random_things/blocks/UnstableStone.java
@@ -1,18 +1,24 @@
 package lol.roxxane.random_things.blocks;
 
-import lol.roxxane.random_things.Rt;
+import lol.roxxane.random_things.tags.RtBlockTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-// TODO: Sfx when walking on. From https://pixabay.com/sound-effects/search/rocks-falling/
+import static lol.roxxane.random_things.config.RtServerConfig.UNSTABLE_STONE_COLLAPSE_SPREAD_CHANCE;
+import static lol.roxxane.random_things.config.RtServerConfig.UNSTABLE_STONE_MAX_COLLAPSE_SIZE;
+
 public class UnstableStone extends Block {
 	private static final List<BlockPos> SHATTER_POSITIONS = List.of(
 		new BlockPos(-1, 1, 0),
@@ -39,65 +45,71 @@ public class UnstableStone extends Block {
 		super(pProperties);
 	}
 
-	public static void spread_crumble(Level level, BlockPos pos, double break_chance, int iteration) {
-		if (break_chance >= level.random.nextFloat())
-			for (var offset : SHATTER_POSITIONS) {
-				var break_state = level.getBlockState(pos.offset(offset));
-				var break_pos = pos.offset(offset);
+	public static void gather_crumble(Level level, BlockPos pos, int iteration, Set<BlockPos> poses) {
+		if (iteration >= UNSTABLE_STONE_MAX_COLLAPSE_SIZE.get())
+			return;
 
-				if (break_state.getBlock() instanceof UnstableStone && iteration <= 3) {
-					level.destroyBlock(break_pos, true);
-					spread_crumble(level, break_pos, 0.333333333, iteration + 1);
-				}
+		for (var offset : SHATTER_POSITIONS) {
+			var collapse_pos = pos.offset(offset);
+			var collapse_state = level.getBlockState(collapse_pos);
+
+			if (UNSTABLE_STONE_COLLAPSE_SPREAD_CHANCE.get() >= level.random.nextFloat() &&
+				(collapse_state.getBlock() instanceof UnstableStone ||
+					collapse_state.is(RtBlockTags.UNSTABLE_STONE_OTHER_COLLAPSE)))
+			{
+				poses.add(collapse_pos);
+
+				if (collapse_state.getBlock() instanceof UnstableStone)
+					gather_crumble(level, collapse_pos, iteration + 1, poses);
 			}
+		}
 	}
 
-	/*
+	public static void crumble(Level level, BlockPos pos) {
+		if (level.isClientSide)
+			return;
+
+		var poses = new HashSet<BlockPos>();
+		poses.add(pos);
+		gather_crumble(level, pos, 1, poses);
+
+		for (var break_pos : poses)
+			level.destroyBlock(break_pos, true);
+	}
+
 	@Override
 	public void fallOn(@NotNull Level level, @NotNull BlockState state, @NotNull BlockPos pos, @NotNull Entity entity,
-		float fall_distance) {
-		if (!level.isClientSide && fall_distance / (entity.isShiftKeyDown() ? 6.0 : 3.0) >= level.random.nextFloat()
-			&& !(entity instanceof ItemEntity)) {
-			level.destroyBlock(pos, true);
-			spread_crumble(level, pos, 1, 1);
-		}
-
+		float fall_distance)
+	{
+		if (fall_distance / 2.0 >= level.random.nextFloat() && !(entity instanceof ItemEntity))
+			crumble(level, pos);
 		super.fallOn(level, state, pos, entity, fall_distance);
 	}
-	*/
 
 	// More like onEntityStanding
 	@Override
 	public void stepOn(@NotNull Level level, @NotNull BlockPos pos, @NotNull BlockState state,
-		@NotNull Entity entity) {
-
-		if (!level.isClientSide)
-			Rt.log("EEEEEEEEEEEEEE");
-
-		// Can't figure out player delta because STUPID so whatever, it'll just randomly break under you
-		if (!level.isClientSide &&
-			(entity.isSprinting() ? 0.05 : 0.01) >= level.random.nextFloat()) {
-			level.destroyBlock(pos, true);
-			spread_crumble(level, pos, 1, 1);
-		}
-
+		@NotNull Entity entity)
+	{
+		// Can't get player delta because STUPID so whatever, it'll just randomly break under you
+		if ((entity.isSprinting() ? 0.05 : 0.01) >= level.random.nextFloat() && !(entity instanceof ItemEntity))
+			crumble(level, pos);
 		super.stepOn(level, pos, state, entity);
 	}
 
 	@Override
 	public void playerWillDestroy(@NotNull Level level, @NotNull BlockPos pos, @NotNull BlockState state,
-		@NotNull Player player) {
+		@NotNull Player player)
+	{
 		super.playerWillDestroy(level, pos, state, player);
 
-		if (!level.isClientSide)
-			spread_crumble(level, pos, 1, 1);
+		if (!EnchantmentHelper.hasSilkTouch(player.getMainHandItem()))
+			crumble(level, pos);
 	}
 
 	@Override
 	public void wasExploded(@NotNull Level level, @NotNull BlockPos pos, @NotNull Explosion explosion) {
 		super.wasExploded(level, pos, explosion);
-
-		if (!level.isClientSide)
-			spread_crumble(level, pos, 1, 1);
+		crumble(level, pos);
 	}
 }
